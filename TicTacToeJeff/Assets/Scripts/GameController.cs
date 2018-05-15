af08661fa5b6d6b8a65aac73c026ea4536cd9e78
Hyacinthe <jefferson-thierry.hyacinthe.1@ens.etsmtl.ca>
2018-05-14 20:58:30 -0400

    On last section of tutorial

---
+++ TicTacToeJeff/Assets/Scripts/GameController.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[System.Serializable]
+public class Player
+{
+    public Image panel;
+    public Text text;
+}
+
+[System.Serializable]
+public class PlayerColor
+{
+    public Color panelColor;
+    public Color textColor;
+}
+
+
+public class GameController : MonoBehaviour{
+
+    public Text[] buttonList;
+    private string playerSide;
+    public GameObject gameOverPanel;
+    public Text gameOverText;
+    private int moveCount;
+    public GameObject restartButton;
+
+    public Player playerX;
+    public Player playerO;
+    public PlayerColor activePlayerColor;
+    public PlayerColor inactivePlayerColor;
+
+    void SetGameControllerReferenceOnButtons()
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+        }
+    }
+
+    void Awake()
+    {
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        playerSide = "X";
+        SetGameControllerReferenceOnButtons();
+        restartButton.SetActive(false);
+        SetPlayerColors(playerX, playerO);
+    }
+
+
+    public string GetPlayerSide()
+    {
+        return playerSide;
+    }
+
+    public void EndTurn()
+    {
+        //Debug.Log("EndTurn is not implemented!");
+
+        moveCount++;
+
+        if (buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[6].text == playerSide && buttonList[7].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[3].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[1].text == playerSide && buttonList[4].text == playerSide && buttonList[7].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[5].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[0].text == playerSide && buttonList[4].text == playerSide && buttonList[8].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+        if (buttonList[2].text == playerSide && buttonList[4].text == playerSide && buttonList[6].text == playerSide)
+        {
+            GameOver(playerSide);
+        }
+
+
+        if (moveCount >= 9)
+        {
+            gameOverPanel.SetActive(true);
+            gameOverText.text = "It's a draw!";
+            GameOver("draw");
+        }
+
+        ChangeSides();
+
+    }
+
+
+    public void RestartGame()
+    {
+        playerSide = "X";
+        moveCount = 0;
+        gameOverPanel.SetActive(false);
+        restartButton.SetActive(false);
+        SetPlayerColors(playerX, playerO);
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = true;
+            buttonList[i].text = "";
+        }
+
+       
+    }
+
+
+    void ChangeSides()
+    {
+        playerSide = (playerSide == "X") ? "O" : "X";
+
+        if (playerSide == "X")
+        {
+            SetPlayerColors(playerX, playerO);
+        }
+        else
+        {
+            SetPlayerColors(playerO, playerX);
+        }
+    }
+
+    void SetPlayerColors(Player newPlayer, Player oldPlayer)
+    {
+        newPlayer.panel.color = activePlayerColor.panelColor;
+        newPlayer.text.color = activePlayerColor.textColor;
+        oldPlayer.panel.color = inactivePlayerColor.panelColor;
+        oldPlayer.text.color = inactivePlayerColor.textColor;
+    }
+
+    void GameOver(string winningPlayer)
+    {
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            buttonList[i].GetComponentInParent<Button>().interactable = false;
+        }
+
+        if (winningPlayer == "draw"){
+            gameOverPanel.SetActive(true);
+            gameOverText.text = "Its a draw";
+        } else { 
+            gameOverPanel.SetActive(true);
+            gameOverText.text = playerSide + " Wins Yeah !!!";
+            
+        }
+
+        restartButton.SetActive(true);
+    }
+}
